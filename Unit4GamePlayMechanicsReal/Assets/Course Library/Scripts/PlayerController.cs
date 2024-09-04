0326177940e2beeece1863d1d8e5e2329b823a53
Xavier Real <720838@student.dusd.net>
2024-09-04 15:27:11 -0700

    Had to reset cause of glitch :(
    Had to reset cause of glitch :( doing 4.3

--- Unit4GamePlayMechanicsReal/Assets/Course Library/Scripts/PlayerController.cs
+++ Unit4GamePlayMechanicsReal/Assets/Course Library/Scripts/PlayerController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject powerupIndicator;
     private GameObject focalPoint;
     private Rigidbody playerRb;
     public float speed = 5.0f;
+    public bool hasPowerup;
+    private float powerupStrength = 15.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +20,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        powerupIndicator.transform.positon = transform.position + new Vector3(0, -0.5f, 0);
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(powerupCountdownRoutine);
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+    private void OnCollisionEneter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<enemyRigidbody>();
+            Debug.Log("Collided with " + collision.gameObject.name + " with powerup to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse)
+        }
     }
 }
